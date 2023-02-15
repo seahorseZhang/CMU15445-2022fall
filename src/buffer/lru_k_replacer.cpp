3c4910ff957b49c28c697d72285c42c1ea787a65
“seahorseZhang” <17212020057@fudan.edu.cn>
2023-02-15 22:10:13 +0800

    Fix bug for online test, the record access for k = 3, and the remove logic, concurrency

--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -17,10 +17,12 @@ namespace bustub {
 LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {}
 
 auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
+  latch_.lock();
   for (auto iter = temp_pool_.begin(); iter != temp_pool_.end();) {
     if ((*iter)->IsEvictable()) {
       *frame_id = (*iter)->GetId();
       iter = temp_pool_.erase(iter);
+      latch_.unlock();
       return true;
     }
     iter++;
@@ -29,18 +31,23 @@ auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
     if ((*iter)->IsEvictable()) {
       *frame_id = (*iter)->GetId();
       iter = cache_pool_.erase(iter);
+      latch_.unlock();
       return true;
     }
     iter++;
   }
+  latch_.unlock();
   return false;
 }
 
 void LRUKReplacer::RecordAccess(frame_id_t frame_id) {
+  BUSTUB_ASSERT(frame_id <= (int)replacer_size_, "Invalid frame id");
+  latch_.lock();
   for (auto iter = cache_pool_.begin(); iter != cache_pool_.end();) {
     if ((*iter)->GetId() == frame_id) {
       cache_pool_.emplace_back(std::move(*iter));
       iter = cache_pool_.erase(iter);
+      latch_.unlock();
       return;
     }
     iter++;
@@ -52,53 +59,62 @@ void LRUKReplacer::RecordAccess(frame_id_t frame_id) {
       if (((*iter)->GetTimes()) >= k_) {
         cache_pool_.emplace_back(std::move(*iter));
         iter = temp_pool_.erase(iter);
-        return;
       }
+      latch_.unlock();
+      return;
     }
     iter++;
   }
   std::unique_ptr<FrameInfo> frame_ptr = std::make_unique<FrameInfo>(frame_id);
   frame_ptr->IncreaseTimes();
   temp_pool_.push_back(std::move(frame_ptr));
+  latch_.unlock();
 }
 
 void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
+  latch_.lock();
   for (auto &ele : temp_pool_) {
     if (ele->GetId() == frame_id) {
       ele->SetEvictable(set_evictable);
+      latch_.unlock();
       return;
     }
   }
   for (auto &ele : cache_pool_) {
     if (ele->GetId() == frame_id) {
       ele->SetEvictable(set_evictable);
+      latch_.unlock();
       return;
     }
   }
+  latch_.unlock();
 }
 
 void LRUKReplacer::Remove(frame_id_t frame_id) {
-  for (auto &ele : temp_pool_) {
-    if (ele->GetId() == frame_id) {
-      if (ele->IsEvictable()) {
-        temp_pool_.remove(ele);
-        return;
-      }
-      throw frame_id;
+  latch_.lock();
+  for (auto iter = temp_pool_.begin(); iter != temp_pool_.end();) {
+    if ((*iter)->GetId() == frame_id) {
+      BUSTUB_ASSERT((*iter)->IsEvictable(), "Remove unEvictable frame id.");
+      iter = temp_pool_.erase(iter);
+      latch_.unlock();
+      return;
     }
+    iter++;
   }
-  for (auto &ele : cache_pool_) {
-    if (ele->GetId() == frame_id) {
-      if (ele->IsEvictable()) {
-        cache_pool_.remove(ele);
-        return;
-      }
-      throw frame_id;
+  for (auto iter = cache_pool_.begin(); iter != cache_pool_.end();) {
+    if ((*iter)->GetId() == frame_id) {
+      BUSTUB_ASSERT((*iter)->IsEvictable(), "Remove unEvictable frame id.");
+      iter = cache_pool_.erase(iter);
+      latch_.unlock();
+      return;
     }
+    iter++;
   }
+  latch_.unlock();
 }
 
 auto LRUKReplacer::Size() -> size_t {
+  latch_.lock();
   size_t num = 0;
   for (auto &ele : cache_pool_) {
     if (ele->IsEvictable()) {
@@ -110,6 +126,7 @@ auto LRUKReplacer::Size() -> size_t {
       num++;
     }
   }
+  latch_.unlock();
   return num;
 }
 
