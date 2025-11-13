80aa024e2f84886466c58564a80d906755390c41
seahorseZhang <17212020057@fudan.edu.cn>
2025-11-14 00:19:49 +0800

    p3 seq scan

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -14,10 +14,26 @@
 
 namespace bustub {
 
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
+SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : 
+AbstractExecutor(exec_ctx), plan_(plan), table_iter_(nullptr, RID(), nullptr) {}
 
-void SeqScanExecutor::Init() { throw NotImplementedException("SeqScanExecutor is not implemented"); }
+void SeqScanExecutor::Init() {
+    Catalog *catalog = exec_ctx_->GetCatalog();
+    table_info_ = catalog->GetTable(plan_->table_name_);
+    if (!table_info_) {
+        return;
+    }
+    table_iter_ = (table_info_->table_->Begin(exec_ctx_->GetTransaction()));
+}
 
-auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+    if (table_info_ == nullptr ||  table_iter_ == table_info_->table_->End()) {
+        return false;
+    }
+    *tuple = *table_iter_;
+    *rid = table_iter_->GetRid();
+    ++table_iter_;
+    return true;
+}
 
 }  // namespace bustub
