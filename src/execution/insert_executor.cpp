0cfa383ad22e0f03740e25637656d3a700f8f885
seahorseZhang <17212020057@fudan.edu.cn>
2025-11-14 23:44:03 +0800

    p3 insert operator

--- src/execution/insert_executor.cpp
+++ src/execution/insert_executor.cpp
@@ -17,11 +17,39 @@
 namespace bustub {
 
 InsertExecutor::InsertExecutor(ExecutorContext *exec_ctx, const InsertPlanNode *plan,
-                               std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+                               std::unique_ptr<AbstractExecutor> &&child_executor): AbstractExecutor(exec_ctx),
+    plan_(plan), child_executor_(std::move(child_executor)) {}
 
-void InsertExecutor::Init() { throw NotImplementedException("InsertExecutor is not implemented"); }
+void InsertExecutor::Init() {
+    child_executor_->Init();
+}
 
-auto InsertExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool { return false; }
-
+auto InsertExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+    if (is_returned) {
+        return false;
+    }
+    int32_t insert_count = 0;
+    Tuple cur_tuple;
+    RID cur_rid;
+    while (child_executor_->Next(&cur_tuple, &cur_rid)) {
+        Catalog *catalog =  exec_ctx_->GetCatalog();
+        TableInfo *table_info = catalog->GetTable(plan_->TableOid());
+        bool insertRet = table_info->table_->InsertTuple(cur_tuple, &cur_rid, exec_ctx_->GetTransaction());
+        if (!insertRet) {
+            return false;
+        }
+        ++insert_count;
+        std::vector<IndexInfo *> indexes = catalog->GetTableIndexes(table_info->name_);
+        for (IndexInfo* &index: indexes) {
+            Tuple key = cur_tuple.KeyFromTuple(table_info->schema_, index->key_schema_, index->index_->GetKeyAttrs());
+            index->index_->InsertEntry(key, cur_rid, exec_ctx_->GetTransaction());
+        }
+    }
+    std::vector<Value> values;
+    values.emplace_back(TypeId::INTEGER, insert_count);
+    *tuple = Tuple(values, &plan_->OutputSchema());
+    is_returned = true;
+    return true;
+ }
+    
 }  // namespace bustub
--- src/include/execution/executors/insert_executor.h
+++ src/include/execution/executors/insert_executor.h
@@ -57,6 +57,8 @@ class InsertExecutor : public AbstractExecutor {
  private:
   /** The insert plan node to be executed*/
   const InsertPlanNode *plan_;
+  std::unique_ptr<AbstractExecutor> child_executor_;
+  bool is_returned {false};
 };
 
 }  // namespace bustub
